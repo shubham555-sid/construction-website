4a9240806a66982f6decdb5920e8228bd18a20a4
shubham555-sid <shubhamsiddheshwar555@gmail.com>
2024-08-30 12:32:49 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,97 @@
+// =======Show Menu=====
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// Menu show-=====
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// ---Menu hidden---==
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// ====== Remove Menu Mobile=====
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+    const navMenu = document.getElementById("nav-menu");
+    //  when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// =========Change Background Header====
+const bgHeader = () => {
+    const header = document.getElementById("header");
+    //add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add("bg-header")
+        : header.classList.remove("bg-header")
+}
+window.addEventListener('scroll', bgHeader)
+bgHeader()
+
+// =============Swiper Services=======
+const swiperServices = new Swiper('.services__swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// ========Show Scroll Up=======
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+    //when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+scrollUp()
+
+// ===========Scroll Sections Active Link========
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('sctive-link')
+        } else {
+            sectionsClass.classList.remove('sctive-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+
+// ==========Scroll Reveal Animation=====
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400,
+    //reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__content, .services__data, .services__swiper, .footer__container`)
+sr.reveal(`.home__images`, { origin: 'bottom', delay: 1000 })
+sr.reveal(`.about__images, .contact__img`, {origin: 'left'})
+sr.reveal(`.about__data, .contact__data`, {origin: 'right'})
+sr.reveal(`.projects__card`, {interval: 100})
